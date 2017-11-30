9b4492ef3716739c1475edd6071e33b44a28e326
yadiw <32943329+yadiw@users.noreply.github.com>
2017-11-29 17:35:02 -0800

    menu scene

---
+++ Assets/scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Button>().onClick.AddListener(() => { switchScene(); });  
+
+	}
+
+	public void switchScene() {
+		SceneManager.LoadScene ("test3.0");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/UnlockLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockLevel : MonoBehaviour {
+
+	public static UnlockLevel instance = null;
+	public bool[] level;
+
+	void Awake() {
+		instance = this;
+	}
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < level.Length; i++) {
+			GameObject button = GameObject.Find ("ButtonLevel" + (i + 1));
+			button.GetComponent<ChangeScene > ().enabled = level [i];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
