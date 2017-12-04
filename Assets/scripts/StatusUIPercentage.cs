20dfbfcfe32c8b08d21d67603b72e3ec1ba5f7bd
Andrey Butenko <ghostmancer@gmail.com>
2017-12-04 14:36:46 -0800

    Add percentage panel

---
+++ Assets/scripts/StatusUIPercentage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusUIPercentage : MonoBehaviour {
+    public GameMaster gameMaster;
+    float initialValue = 0;
+    
+	void Start () {
+        initialValue = PassValue.emissions;
+    }
+	
+	void Update () {
+        float proportion = (gameMaster.getCurrentEmissions() - initialValue) / (gameMaster.getTargetEmissions() - initialValue);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(150, Screen.height * proportion);
+    }
+}
